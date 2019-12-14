e15291f010ab8e1d14a6168b3a82f6825050fa8e
osharnikov <oleg.sharnikov@kohls.com>
2019-12-14 10:02:32 +0300

    Refactor

---
+++ src/main/scala/test/option/iq/Context.scala
@@ -0,0 +1,14 @@
+package test.option.iq
+
+import java.util.concurrent.ExecutorService
+
+import scala.concurrent.ExecutionContext
+
+trait Context[+E <: ExecutorService] {
+  val executor: E
+  implicit val context = ExecutionContext.fromExecutor(executor)
+}
+
+trait MainContext extends Context[ExecutorService] {
+  override implicit val executor = java.util.concurrent.Executors.newWorkStealingPool()
+}
